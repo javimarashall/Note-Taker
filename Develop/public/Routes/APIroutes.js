37e1caee3d37df88ffd36e30991619ea8ba4c01f
javimarashall <jamondragon@ucdavis.edu>
2021-04-20 21:31:55 -0700

    routes

--- Develop/public/Routes/APIroutes.js
+++ Develop/public/Routes/APIroutes.js
@@ -2,5 +2,13 @@ const dataBase = require("../db/db.json");
 
 //Routing
 module.exports = (app) => {
+    //API GET request
     app.get("/api/notes", (req, res) => res.json(dataBase));
+
+    //API post request
+    app.post("/api/notes", (req, res) => {  //need to add an ID
+        dataBase.push(req.body);        
+        res.json(dataBase); //not sure if this is ok
+    })
+
 }
