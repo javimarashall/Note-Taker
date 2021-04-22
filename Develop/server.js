1bce1e896d7026d403ca99a546d75e6d03af9960
javimarashall <jamondragon@ucdavis.edu>
2021-04-21 18:19:25 -0700

    server

---
+++ Develop/server.js
@@ -0,0 +1,12 @@
+const express = require ("express");
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`app listening on PORT: ${PORT}`);
+});
